fa8066327d4fa38d5c7864f6e41d637382f7a73e
CJT1105 <CJT1105@GMAIL.COM>
2016-10-14 11:58:26 -0500

    added routes to add student and return all students in db

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const knex = require('knex')({
+	client: 'sqlite3',
+	connection: 'dev.sqlite3',
+})
+
+app.use(bodyParser.urlencoded({extened: true}))
+
+app.post('/api/student/create', (req,res) => {
+    const student = {
+        FirstName: req.body.firstname,
+        LastName: req.body.lastname
+    }
+    knex('Students').insert(student)
+    .then(results => console.log(results))
+    res.end()
+	
+})
+
+app.get('/api/students', (req,res) => {
+    knex('Students').select('Students.*')
+    .then(users => res.json(users))
+})
+
+app.listen(3000, () => {
+    console.log('listening on PORT 3000')
+})
---
+++ models/Student.js
@@ -0,0 +1,7 @@
+const config = require('../knexfile').development
+const knex = require('knex')(config)
+const bookshelf = require('bookshelf')(knex)
+
+module.exports = bookshelf.Model.extend({
+	tableName: 'Students'
+})
