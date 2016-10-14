d937d160509d5b33f4798e991f92505f9c8019a3
Alex Horton <alexhortonmusic@gmail.com>
2016-10-14 14:44:14 -0500

    created projects table, project routes, get specific project

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser')
 const knex = require('knex')({
 	client: 'sqlite3',
 	connection: 'dev.sqlite3',
+	useNullAsDefault: true
 })
 
 app.use(bodyParser.urlencoded({extened: true}))
 
+// students
 app.post('/api/students', (req,res) => {
     const student = {
         FirstName: req.body.firstname,
@@ -17,12 +19,36 @@ app.post('/api/students', (req,res) => {
     knex('Students').insert(student)
     .then(results => console.log(results))
     res.end()
-	
+
 })
 
 app.get('/api/students', (req,res) => {
     knex('Students').select('Students.*')
     .then(users => res.json(users))
+})
+
+// projects
+app.post('/api/projects', (req,res) => {
+    const project = {
+      ProjectName: req.body.projectname,
+			StudentId: req.body.studentid
+    }
+    knex('Projects').insert(project)
+    .then(results => console.log(results))
+    res.end()
+})
+
+app.get('/api/projects', (req,res) => {
+    knex('Projects').select('Projects.*')
+    .then(projects => res.json(projects))
+})
+
+app.get('/api/projects/:id', (req, res) => {
+	const id = req.params.id
+	knex('Projects').select('Projects.*').where('Projects.StudentId', id).then(project => {
+		res.json(project)
+		res.end()
+	})
 })
 
 app.listen(3000, () => {
